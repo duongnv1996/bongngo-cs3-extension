887a072918e58a3d3ab3cc1230c4738f95f24cce
duongnv1996 <duongnv1996@gmail.com>
2023-05-24 00:09:25 +0700

    update phimmoi 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ subprojects {
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         // you can modify it to use other git hosting services, like gitlab
-//        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/duongnv1996/phimhd-cloudstream")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/duongnv1996/bongngo-cs3-extension")
         setRepo("https://github.com/cinehubwork/abc/releases/download/prelease")
         overrideUrlPrefix("https://github.com/cinehubwork/abc/releases/download/prelease")
     }
